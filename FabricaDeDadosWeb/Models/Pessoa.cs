987b386e0ede842c53b6208c24c7d601a47717dd
Giovanne da Silva Batista <giovannebatista3@gmail.com>
2020-01-13 00:08:15 -0400

    -Novo Contexto

---
+++ FabricaDeDadosWeb/Models/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FabricaDeDadosWeb.Models
+{
+    public partial class Pessoa
+    {
+        public Pessoa()
+        {
+            PessoaEndereco = new HashSet<PessoaEndereco>();
+        }
+
+        public int Id { get; set; }
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string NomeMae { get; set; }
+        public string NomePai { get; set; }
+        public string Rg { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Celular { get; set; }
+        public string Sexo { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public virtual ICollection<PessoaEndereco> PessoaEndereco { get; set; }
+    }
+}
---
+++ FabricaDeDadosWeb/Models/PessoaEndereco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FabricaDeDadosWeb.Models
+{
+    public partial class PessoaEndereco
+    {
+        public int Id { get; set; }
+        public int IdPessoa { get; set; }
+        public int IdEndereco { get; set; }
+
+        public virtual Endereco IdEnderecoNavigation { get; set; }
+        public virtual Pessoa IdPessoaNavigation { get; set; }
+    }
+}
