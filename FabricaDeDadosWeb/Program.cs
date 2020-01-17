c7ac342ee3bb1b9f51aa1443e9367ac5f879e026
Giovanne da Silva Batista <giovannebatista3@gmail.com>
2020-01-16 23:49:07 -0400

    -Docker

--- FabricaDeDadosWeb/Program.cs
+++ FabricaDeDadosWeb/Program.cs
@@ -21,6 +21,7 @@ namespace FabricaDeDadosWeb
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:80/");
                 });
     }
 }
