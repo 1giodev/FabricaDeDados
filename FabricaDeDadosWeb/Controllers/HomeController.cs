32bef40338f6d96436d964feed09e1c88985ba88
Giovanne da Silva Batista <19177453+eugiioo-dev@users.noreply.github.com>
2020-01-29 16:19:12 -0400

    -Criação do Layout da View do Login
    -Criação do Layout da View do Login

--- FabricaDeDadosWeb/Controllers/HomeController.cs
+++ FabricaDeDadosWeb/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace FabricaDeDadosWeb.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
